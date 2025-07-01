eaf2126ef413d81f0eb9897140b12c41a337df45
Brian Norris <brian.norris@acxion.com>
2025-07-01 07:56:09 -0500

    finish june

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -118,6 +118,12 @@ module.exports = {
         collapsable: true,
         children: [
           "2024/january/",
+          "2024/february/",
+          "2024/march/",
+          "2024/april/",
+          "2024/may/",
+          "2024/june/",
+          "2024/july/",
         ],
       },
     ],
